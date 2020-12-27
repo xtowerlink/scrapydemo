2b927885505250585457cb0a52e605afa840fd7a
xtowerlink <38056310+xtowerlink@users.noreply.github.com>
2020-12-27 13:18:30 +0800

    Update scrapdemo.py

--- scrapdemo.py
+++ scrapdemo.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-
-
 import requests
 from bs4 import BeautifulSoup
-
-
-# In[14]:
 
 
 url = "http://books.toscrape.com/index.html"
@@ -16,69 +10,23 @@ r = r = requests.get(url)
 html = r.content
 scraped = BeautifulSoup(html,"html.parser")
 
-
-# In[18]:
-
-
-scraped
-
-
-# In[22]:
-
-
 scraped.title
-
-
-# In[23]:
-
 
 scraped.find('title')
 
-
-# In[24]:
-
-
 scraped.title.text
-
-
-# In[25]:
-
 
 scraped.title.text.strip()
 
-
-# In[26]:
-
-
 title_text=scraped.title.text.strip()
 
-
-# In[30]:
-
-
 link_text=scraped.article.h3.a.text
-
-
-# In[32]:
 
 
 link_text=scraped.article.h3.a['title']
 
 
-# In[35]:
-
-
 items=scraped.find_all("article",class_="product_pod")
-
-
-# In[36]:
-
-
-items
-
-
-# In[45]:
-
 
 items=scraped.find_all("article",class_="product_pod")
 
@@ -88,21 +36,9 @@ for item in items:
     titles.append(item.h3.a['title'])
 
 
-# In[46]:
-
-
-titles
-
-
-# In[58]:
-
-
 title_links=scraped.find_all("a",title=True)
 for link in title_links:
     print(link["title"])
-
-
-# In[64]:
 
 
 all_divs=scraped.select("div")
